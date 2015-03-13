9f2e4680e4d3f1e9696c1dda1a7760b557db691e
Cristofer <Cristofer@10.68.174.72>
2015-03-13 13:37:45 -0600

    PrimeraSubida

---
+++ src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyecto;
+
+/**
+ *
+ * @author Cristofer
+ */
+public class PrimerProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Compañeros este es el proyecto caa uno cree las clases que acordamos en el paquete prier proyecto
+        System.out.println("Proyecto para compartir");
+    }
+    
+}
